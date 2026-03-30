8769e831bbc0756694790419dbd82b68d15aa6ac
Zoey <kurkaz4709@gmail.com>
2026-03-30 12:17:35 -0500

    Fixed Rich print with print_list

--- tools/print_list.py
+++ tools/print_list.py
@@ -5,19 +5,21 @@ def print_list(items=list, text=str):
     print(text, end=" ")
     
     if len(items) == 2:
-        print(items[0], "and", items[1], end="!\n")
+        print(str(items[0]), "and", str(items[1]), end="!\n")
+        # This wouldn't be necessary if you don't use the Oxford comma, just remove the comma from 2nd to last item below
         return
 
     for itm in items:
         
         if (len(items) - 1) - items.index(itm) >= 2:
-            # e.g., member 0 or 1 of 4
-            print(itm, end=", ")
+            # e.g., item 0 or 1 of 4
+            print(str(itm), end=", ")
         
         if (len(items) - 1) - items.index(itm) == 1:
-            # e.g., member 2 of 3 or 3 of 4
-            print(itm, end=", and ")
+            # e.g., item 2 of 3 or 3 of 4
+            print(str(itm), end=", and ")
+            # Oxford comma
         
         if (len(items) - 1) - items.index(itm) == 0:
-            # last member of party
-            print(itm, end="!\n")
+            # last item in list
+            print(str(itm), end="!\n")
